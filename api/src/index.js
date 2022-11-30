7c5f94bd8d2f818b5461b69644e6e95160b5a6ff
Jan Behrens <36447927+janbehrens@users.noreply.github.com>
2022-11-30 12:35:25 +0100

    add objections api

--- api/src/index.js
+++ api/src/index.js
@@ -97,6 +97,48 @@ app.post('/publicreviews', jsonParser, async (req, res) => {
   }
 })
 
+app.get('/publicreviews/:id/objections', async (req, res) => {
+  await client.connect()
+
+  try {
+    const result = await client.db(dbname).collection('objections').find({
+      publicReview: req.params.id
+    }).toArray()
+    res.json(result)
+  } catch (err) {
+    res.status(400).send('Error fetching objections')
+  }
+})
+
+app.post('/publicreviews/:id/objections', jsonParser, async (req, res) => {
+  await client.connect()
+
+  // check that public review exists
+  try {
+    if (!await client.db(dbname).collection('publicreviews').findOne(ObjectId(req.body.publicReview))) {
+      throw new Error()
+    }
+  } catch (err) {
+    res.status(404).send(`Error: public review with ID ${req.body.publicReview} not found`)
+    return
+  }
+
+  const document = {
+    publicReview: req.body.publicReview,
+    text: req.body.text,
+    author: req.body.author,
+    coordinate: req.body.coordinate,
+    created: new Date()
+  }
+
+  try {
+    await client.db(dbname).collection('objections').insertOne(document)
+    res.status(204).send()
+  } catch (err) {
+    res.status(400).send('Error creating objection')
+  }
+})
+
 
 // start listening
 app.listen(port, () => {
